6ee645286e13a5f6dd407e8716c48441e1951f59
paco <paco.barbudo@gmail.com>
2013-09-29 22:07:59 +0200

    add a conflict comment line

--- GradleGitProyect/src/main/java/org/gradle/Person.java
+++ GradleGitProyect/src/main/java/org/gradle/Person.java
@@ -8,6 +8,7 @@ public class Person {
     public Person(String name) {
         this.name = name;
         new GrowthList();
+        // conflict comment line
     }
 
     public String getName() {
